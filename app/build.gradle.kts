4e7428831e882ca9977b8abd4e1a47e3660fb01a
fikriks <fikriks130@gmail.com>
2024-03-19 22:25:46 +0700

    praktikum modul 2:rocket:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.fikriks.praktikum_pab"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.fikriks.praktikum_pab"
@@ -32,6 +32,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 }
 
--- app/src/main/java/com/fikriks/praktikum_pab/MainActivity.kt
+++ app/src/main/java/com/fikriks/praktikum_pab/MainActivity.kt
@@ -2,10 +2,40 @@ package com.fikriks.praktikum_pab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.fikriks.praktikum_pab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.hitung.setOnClickListener{
+            val name = binding.nama.text.toString()
+            binding.outputNama.text = name
+
+            val gajiStatus: Int = if (binding.menikah.isChecked) {
+                500000
+            } else {
+                0
+            }
+
+            val golongan = binding.rgGolongan
+            val gol = golongan.checkedRadioButtonId
+            val gajiGolongan: Int = when (gol) {
+                R.id.rbGolongan1 -> 1000000
+                R.id.rbGolongan2 -> 2000000
+                else -> 0
+            }
+
+            val totalGaji = gajiStatus + gajiGolongan
+            binding.outputGaji.text = totalGaji.toString()
+            binding.outputStatus.text = if(binding.menikah.isChecked) { "Sudah Menikah" } else { "Belum Menikah" }
+            binding.outputGolongan.text = if(binding.rbGolongan1.isChecked) { "Golongan 1" } else { "Golongan 2" }
+            binding.outputHitung.visibility = View.VISIBLE
+        }
     }
 }
