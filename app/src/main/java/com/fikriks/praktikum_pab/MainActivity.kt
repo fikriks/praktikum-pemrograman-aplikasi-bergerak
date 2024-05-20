420daf6e009d5a300be07ebef1e8b0867784dc02
fikriks <fikriks130@gmail.com>
2024-05-20 16:33:10 +0700

    tugas modul 7 :rocket:
    menerapkan sensor

--- app/src/main/java/com/fikriks/praktikum_pab/MainActivity.kt
+++ app/src/main/java/com/fikriks/praktikum_pab/MainActivity.kt
@@ -1,38 +1,105 @@
 package com.fikriks.praktikum_pab
 
+import android.content.Context
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import com.fikriks.praktikum_pab.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var sensorManager: SensorManager
+    private var lightSensor: Sensor? = null
+    private lateinit var tvLight: TextView
+    private lateinit var cameraManager: CameraManager
+    private var cameraId: String? = null
+    private lateinit var flashRunnable: Runnable
+    private var flashOn: Boolean = false
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
 
-        binding.loginFooterButton.setOnClickListener{
-            val loginFragment: Fragment = LoginFragment()
-            loadFragment(loginFragment)
+        tvLight = binding.tvTemperature
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        binding.relativeLayout.setBackgroundColor(Color.BLACK);
+        tvLight.text = "Lampu Menyala"
+
+        try {
+            cameraId = cameraManager.cameraIdList[0]
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
 
-        binding.registerFooterButton.setOnClickListener{
-            val registerFragment: Fragment = RegisterFragment()
-            loadFragment(registerFragment)
+        if (lightSensor == null) {
+            Toast.makeText(this, "Sensor cahaya tidak tersedia", Toast.LENGTH_SHORT).show()
         }
 
         setContentView(view)
     }
 
-    private fun loadFragment(fragment: Fragment) {
-        val fm: FragmentManager = supportFragmentManager
-        val fragmentTransaction: FragmentTransaction = fm.beginTransaction()
-        fragmentTransaction.replace(binding.fragmentContainer.id, fragment)
-        fragmentTransaction.commit()
+    override fun onResume() {
+        super.onResume()
+        lightSensor?.also { sensor ->
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // We do not need to handle accuracy changes for this example
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_LIGHT) {
+            var light = event.values[0]
+            if(light.toInt() == 0){
+                binding.relativeLayout.setBackgroundColor(Color.WHITE);
+                tvLight.text = "Lampu Sekitar Mati"
+                startFlash()
+            } else {
+                binding.relativeLayout.setBackgroundColor(Color.BLACK);
+                tvLight.text = "Lampu Sekitar Menyala"
+                stopFlash()
+            }
+        }
+    }
+
+    private fun startFlash() {
+        try {
+            // Nyalakan lampu kilat
+            cameraManager.setTorchMode(cameraId!!, true)
+        } catch (e: CameraAccessException) {
+            // Tangani jika terjadi akses kamera gagal
+            e.printStackTrace()
+        }
+    }
+
+    private fun stopFlash() {
+        try {
+            // Matikan lampu kilat
+            cameraManager.setTorchMode(cameraId!!, false)
+        } catch (e: CameraAccessException) {
+            // Tangani jika terjadi akses kamera gagal
+            e.printStackTrace()
+        }
     }
 }
